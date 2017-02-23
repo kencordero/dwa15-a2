23cd2b22f75492d84ac9418e4f9b9e2bd48963ca
Kenneth Cordero <kencordero@gmail.com>
2017-02-23 17:07:47 -0600

    Removed unused stylesheet

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
 <link rel="shortcut icon" type="'image/x-icon" href="/images/favicon-shopping-cart.ico">
 
 <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootswatch/3.3.7/united/bootstrap.min.css">
-<link rel="stylesheet" href="/css/styles.css">
+<!--<link rel="stylesheet" href="/css/styles.css">-->
 <div class="container">
     <form method="GET" action="index.php">
         <fieldset class="checkboxes">
