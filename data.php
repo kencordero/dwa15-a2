6bda6789816e9eea9e7fa0b45bdff5381d2e0ddf
Kenneth Cordero <kencordero@gmail.com>
2017-02-22 22:15:16 -0600

    import data from JSON files

--- data.php
+++ data.php
@@ -1,33 +1,6 @@
 <?php
-$states = [ 'AL' => 'Alabama', 'AK' => 'Alaska', 'AZ' => 'Arizona',
-    'AR' => 'Arkansas', 'CA' => 'California', 'CO' => 'Colorado',
-    'CT' => 'Connecticut', 'DE' => 'Delaware', 'FL' => 'Florida',
-    'GA' => 'Georgia', 'HI' => 'Hawaii', 'ID' => 'Idaho', 'IL' => 'Illinois',
-    'IN' => 'Indiana', 'IA' => 'Iowa', 'KS' => 'Kansas', 'KY' => 'Kentucky',
-    'LA' => 'Louisiana', 'ME' => 'Maine', 'MD' => 'Maryland',
-    'MA' => 'Massachusetts', 'MI' => 'Michigan', 'MN' => 'Minnesota',
-    'MS' => 'Mississippi', 'MO' => 'Missouri', 'MT' => 'Montana',
-    'NE' => 'Nebraska', 'NV' => 'Nevada', 'NH' => 'New Hampshire',
-    'NJ' => 'New Jersey', 'NM' => 'New Mexico', 'NY' => 'New York',
-    'NC' => 'North Carolina', 'ND' => 'North Dakota', 'OH' => 'Ohio',
-    'OK' => 'Oklahoma', 'OR' => 'Oregon', 'PA' => 'Pennsylvania',
-    'RI' => 'Rhode Island', 'SC' => 'South Carolina', 'SD' => 'South Dakota',
-    'TN' => 'Tennessee', 'TX' => 'Texas', 'UT' => 'Utah', 'VT' => 'Vermont',
-    'VA' => 'Virginia', 'WA' => 'Washington', 'WV' => 'West Virginia',
-    'WI' => 'Wisconsin', 'WY' => 'Wyoming'
-];
 
-$apparel = [
-    'shirt' => 19.99,
-    'pants' => 29.99,
-    'hat' => 9.99,
-    'socks' => number_format(5.00, 2),
-    'shoes' => 79.99
-];
-
-$shippingMethods = [
-    'same day' => 14.99,
-    'one day' => 9.99,
-    'two day' => 4.99,
-    'some day' => number_format(0.00, 2)
-];
+// Sales Tax Rates Source: https://en.wikipedia.org/wiki/Sales_taxes_in_the_United_States
+$states = json_decode(file_get_contents('data/usStates.json'), true);
+$inventory = json_decode(file_get_contents('data/inventory.json'), true);
+$shippingMethods = json_decode(file_get_contents('data/shippingMethods.json'), true);
